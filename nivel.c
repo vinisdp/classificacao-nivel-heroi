0b150063f8c95f60fb925945613d5e7e9849c9bc
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-10-23 15:18:17 -0400

    Create nivel.c

---
+++ nivel.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+struct Heroi {
+  char nome[50];
+  int xp;
+  char nivel[50];
+};
+typedef struct Heroi heroi;
+int main(void) {
+  heroi heroi;
+  scanf("%s", heroi.nome);
+  scanf("%d", &heroi.xp);
+
+  if (heroi.xp <= 1000) {
+    strcpy(heroi.nivel, "Ferro");
+  } else if (heroi.xp > 1000 && heroi.xp <= 2000) {
+    strcpy(heroi.nivel, "Bronze");
+  } else if (heroi.xp > 2000 && heroi.xp <= 5000) {
+    strcpy(heroi.nivel, "Prata");
+  } else if (heroi.xp > 5000 && heroi.xp <= 7000) {
+    strcpy(heroi.nivel, "Ouro");
+  } else if (heroi.xp > 7000 && heroi.xp <= 8000) {
+    strcpy(heroi.nivel, "Platina");
+  } else if (heroi.xp > 8000 && heroi.xp <= 9000) {
+    strcpy(heroi.nivel, "Ascendente");
+  } else if (heroi.xp > 9000 && heroi.xp <= 10000) {
+    strcpy(heroi.nivel, "Imortal");
+  } else {
+    strcpy(heroi.nivel, "Radiante");
+  }
+  printf("O Herói de nome %s está no nível de %s", heroi.nome, heroi.nivel);
+}
